90ffbd0ae4d91f2fe42878b5fc3c0768c5b6e683
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-02 19:49:34 -0200

    initial App component structure and restructuring of the files

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './index.css'
+import AdCard from '../AdCard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">McMakler - frontend code test</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <AdCard />
+        </main>
       </div>
     )
   }
