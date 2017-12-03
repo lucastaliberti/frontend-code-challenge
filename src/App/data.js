8932d573d823fd0decffe7cb9f8dacc0991f6b9c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-03 00:39:10 -0200

    Data retrieval function

---
+++ src/App/data.js
@@ -0,0 +1,26 @@
+const Ads = async () => {
+  const json = await fetch(
+    'https://cors-anywhere.herokuapp.com/https://api.mcmakler.de/v1/advertisements'
+  ).then(response => response.json())
+
+  console.log(json.data.slice(0, 10).map((entry, index) => entry))
+  return json.data.slice(0, 10).map((entry, index) => ({
+    type: entry.purpose === 0 ? 'Mieten' : 'Kaufen',
+    image: entry.advertisementAssets[0]
+      ? entry.advertisementAssets[0].advertisementThumbnails.inventory_m.url
+      : entry.advertisementAssets.advertisementThumbnails.inventory_m.url,
+    description: entry.title,
+    location: entry.realestateSummary.address.fullAddress
+      ? entry.realestateSummary.address.fullAddress
+      : `${entry.realestateSummary.address.street}  ${
+          entry.realestateSummary.address.number
+        }, ${entry.realestateSummary.address.postalCode} ${
+          entry.realestateSummary.address.city
+        }`,
+    price: entry.advertisementPrice.sellPrice.toFixed(2) + ' €',
+    rooms: entry.realestateSummary.numberOfRooms + ' Zimmer',
+    measure: `ab ${entry.realestateSummary.space.toFixed(2)}m2`
+  }))
+}
+
+export default Ads
