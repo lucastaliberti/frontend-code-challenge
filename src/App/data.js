30b16206b56b9591a1a92ddb9472a32124b4254d
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-03 14:10:59 -0200

    Reworked data util file

--- src/App/data.js
+++ src/App/data.js
@@ -1,26 +1,25 @@
-const Ads = async () => {
-  const json = await fetch(
+export const fetchAdsFromApi = async () => {
+  return await fetch(
     'https://cors-anywhere.herokuapp.com/https://api.mcmakler.de/v1/advertisements'
   ).then(response => response.json())
-
-  console.log(json.data.slice(0, 10).map((entry, index) => entry))
-  return json.data.slice(0, 10).map((entry, index) => ({
-    type: entry.purpose === 0 ? 'Mieten' : 'Kaufen',
-    image: entry.advertisementAssets[0]
-      ? entry.advertisementAssets[0].advertisementThumbnails.inventory_m.url
-      : entry.advertisementAssets.advertisementThumbnails.inventory_m.url,
-    description: entry.title,
-    location: entry.realestateSummary.address.fullAddress
-      ? entry.realestateSummary.address.fullAddress
-      : `${entry.realestateSummary.address.street}  ${
-          entry.realestateSummary.address.number
-        }, ${entry.realestateSummary.address.postalCode} ${
-          entry.realestateSummary.address.city
-        }`,
-    price: entry.advertisementPrice.sellPrice.toFixed(2) + ' €',
-    rooms: entry.realestateSummary.numberOfRooms + ' Zimmer',
-    measure: `ab ${entry.realestateSummary.space.toFixed(2)}m2`
-  }))
 }
 
-export default Ads
+export const adsDataMapper = (entry, index) => ({
+  type: entry.purpose === 0 ? 'Mieten' : 'Kaufen',
+  image: entry.advertisementAssets[0]
+    ? entry.advertisementAssets[0].advertisementThumbnails.inventory_m.url
+    : entry.advertisementAssets.advertisementThumbnails.inventory_m.url,
+  description: entry.title,
+  location: entry.realestateSummary.address.fullAddress
+    ? entry.realestateSummary.address.fullAddress
+    : `${entry.realestateSummary.address.street}  ${
+        entry.realestateSummary.address.number
+      }, ${entry.realestateSummary.address.postalCode} ${
+        entry.realestateSummary.address.city
+      }`,
+  price: entry.advertisementPrice.sellPrice.toFixed(2) + ' €',
+  rooms: entry.realestateSummary.numberOfRooms + ' Zimmer',
+  measure: `ab ${entry.realestateSummary.space.toFixed(2)}m2`
+})
+
+export const adsDataParser = json => json.data.slice(0, 10).map(adsDataMapper)
