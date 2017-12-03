2468de15e97babdb18cc523413c2e26f4ec3e700
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-03 14:10:19 -0200

    Adding missing types and actions

--- src/App/actions.js
+++ src/App/actions.js
@@ -1,5 +1,15 @@
-import { DATA_RETRIEVED } from './types'
+import { DATA_REQUEST, DATA_RETRIEVED, DATA_RETRIEVE_FAILED } from './types'
+
+export const dataRequest = data => ({
+  type: DATA_REQUEST
+})
+
 export const dataRetrieved = data => ({
   type: DATA_RETRIEVED,
   payload: data
+})
+
+export const dataRetrieveFailed = error => ({
+  type: DATA_RETRIEVE_FAILED,
+  payload: error
 })
