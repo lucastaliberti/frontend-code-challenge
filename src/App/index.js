97b8156f7eee02467123d9008b86b587e2474944
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-12-03 00:40:38 -0200

    Plugged the data retrieval on the main App component

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react'
 import './index.css'
 import AdCard from '../AdCard'
+import Ads from './data'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      ads: []
+    }
+  }
+
+  componentDidMount() {
+    Ads().then(ads => {
+      this.setState({
+        ads: ads
+      })
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">McMakler - frontend code test</h1>
         </header>
         <main>
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
-          <AdCard />
+          {this.state.ads.map((ad, index) => <AdCard key={index} ad={ad} />)}
         </main>
       </div>
     )
